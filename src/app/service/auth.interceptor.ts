492d9d5fd96e751c7d05174b3b6ba7fed720d20c
saravanakumare5 <saravana.kumar@e5.ai>
2025-02-06 17:39:29 +0530

    auth interceptor

--- src/app/service/auth.interceptor.ts
+++ src/app/service/auth.interceptor.ts
@@ -6,28 +6,27 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-//import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(/*private authService: AuthService*/) {}
+  constructor() { }
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    // const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpheWRlZXAgUGF0aWwiLCJpYXQiOjE1MTYyMzkwMjJ9.yt3EOXf60R62Mef2oFpbFh2ihkP5qZ4fM8bjVnF8YhA";//his.authService.getToken();
-    console.log("interception is calling");
-    // if (authToken) {
-    //   // Clone the request and attach the token
-    //   const authReq = req.clone({
-    //     setHeaders: {
-    //       Authorization: `Bearer ${authToken}`
-    //     }
-    //   });
-
-    //   return next.handle(authReq);
-    // }
+    let authToken: any = localStorage.getItem('jwt');
+    if (authToken) {
+      
+      const authReq = req.clone({
+        setHeaders: {
+          'Content-Type': 'application/json',
+          Authorization: `${authToken}`
+        }
+      });
+      console.log(req.headers);
+      return next.handle(authReq);
+    }
 
     // If there is no token, pass the original request
     return next.handle(req);
--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,20 +7,15 @@ import { HttpClient, HttpHeaders  } from '@angular/common/http';
 export class HttpService {
 
   baseURL = 'http://localhost:5000'
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
 
 
   public postData(url: string, data: object): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
-        let token: any = localStorage.getItem('jwt');
-        const headers = new HttpHeaders({
-          'Content-Type': 'application/json',  // Set Content-Type header
-          'Authorization': token,  // Set Authorization header (if needed)
-        });
-        this.http.post(this.baseURL + url, data, {headers}).subscribe((response: any) => {
-            resolve(response);
+        this.http.post(this.baseURL + url, data).subscribe((response: any) => {
+          resolve(response);
         }, (err) => {
           console.log(err)
           reject(err);
@@ -35,14 +30,8 @@ export class HttpService {
   public getData(url: string): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
-        let token: any = localStorage.getItem('jwt');
-        console.log("token", token);
-        const headers = new HttpHeaders({
-          'Content-Type': 'application/json',  // Set Content-Type header
-          Authorization: token,  // Set Authorization header (if needed)
-        });
-        this.http.get(this.baseURL + url, {headers}).subscribe((response: any) => {
-            resolve(response);
+        this.http.get(this.baseURL + url).subscribe((response: any) => {
+          resolve(response);
         }, (err) => {
           console.log("err", err);
           reject(err);
